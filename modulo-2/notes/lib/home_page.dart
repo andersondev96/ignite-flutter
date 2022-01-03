70665eb681139620d5123f3cb46c3831684f56de
Anderson Fernandes <andersonfferreira13@hotmail.com>
2022-01-03 11:51:52 -0300

    Creating notes string list

---
+++ modulo-2/notes/lib/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var notes = <String>["Primeiro item"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("NOTES"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            for (var i = 0; i < notes.length; i++)
+              Card(
+                child: ListTile(
+                  title: Text(notes[i]),
+                ),
+              ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          notes.add("Item ${notes.length}");
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
