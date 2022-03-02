5eb0426c41e3ab5be95cc11ea40d1a0423e674de
Anderson Fernandes <andersonfferreira13@hotmail.com>
2022-03-02 10:03:43 -0300

    Creating state management

---
+++ gerencia_de_estado/test/gerencia_de_estado_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gerencia_de_estado/gerencia_de_estado.dart';
+
+void main() {
+  late GerenciaDeEstado controller;
+
+  setUp(() {
+    controller = GerenciaDeEstado();
+  });
+
+  test("Testando a gerência de estado", () {
+    expect(controller.state, isNull);
+    controller.listen((state) {
+      expect(controller.state, "alterado");
+    });
+    controller.setState("alterado");
+    expect(controller.state, "alterado");
+  });
+}
